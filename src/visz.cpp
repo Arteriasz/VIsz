5a1a2bdf6c3699a4dd1761a814342a10d8f3702b
Arthur3040 <arthurnunesdb1@gmail.com>
2025-02-11 19:55:52 -0300

    Cursor start from above

--- src/visz.cpp
+++ src/visz.cpp
@@ -51,7 +51,7 @@ int main() {
         exit(1);
     }
 
-    cout << "\033[2J \033[0d \033[0G";
+    cout << "\033[2J \033[d \033[G";
     while(cin.read(&input, 1)){
         if(input == 3) {
             raise(SIGINT);
